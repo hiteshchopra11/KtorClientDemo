331fbc47eef8e1bcd714a194efbe434df7430b13
Hitesh Chopra <34310832+hiteshchopra11@users.noreply.github.com>
2022-11-09 11:43:21 +0530

    Migrated from ktor-client verison 1.6.7 to 2.1.3

--- app/src/main/java/com/hiteshchopra/ktorclientdemo/data/service/ImageService.kt
+++ app/src/main/java/com/hiteshchopra/ktorclientdemo/data/service/ImageService.kt
@@ -2,28 +2,24 @@ package com.hiteshchopra.ktorclientdemo.data.service
 
 import com.hiteshchopra.ktorclientdemo.data.model.ImageResponse
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.engine.android.Android
-import io.ktor.client.features.json.GsonSerializer
-import io.ktor.client.features.json.JsonFeature
-import io.ktor.client.features.logging.Logging
-import kotlinx.serialization.ExperimentalSerializationApi
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.gson.gson
 
 interface ImageService {
-  suspend fun getImages(): ImageResponse
+    suspend fun getImages(): ImageResponse
 
-  companion object {
-    @ExperimentalSerializationApi fun create(): ImageService {
-      return ImageServiceImpl(
-        client = HttpClient(Android) {
-          install(Logging)
-          install(JsonFeature) {
-            serializer = GsonSerializer() {
-              setPrettyPrinting()
-              disableHtmlEscaping()
-            }
-          }
+    companion object {
+        fun create(): ImageService {
+            return ImageServiceImpl(
+                client = HttpClient(Android) {
+                    install(Logging)
+                    install(ContentNegotiation) {
+                        gson()
+                    }
+                }
+            )
         }
-      )
     }
-  }
 }
--- app/src/main/java/com/hiteshchopra/ktorclientdemo/data/service/ImageServiceImpl.kt
+++ app/src/main/java/com/hiteshchopra/ktorclientdemo/data/service/ImageServiceImpl.kt
@@ -4,20 +4,22 @@ import com.hiteshchopra.ktorclientdemo.BuildConfig
 import com.hiteshchopra.ktorclientdemo.data.HttpRoutes
 import com.hiteshchopra.ktorclientdemo.data.model.ImageResponse
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.request.headers
 import io.ktor.client.request.url
 import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
 
 class ImageServiceImpl(
-  private val client: HttpClient
+    private val client: HttpClient
 ) : ImageService {
-  override suspend fun getImages(): ImageResponse {
-    return client.get {
-      headers {
-        append(HttpHeaders.Authorization, "Client-ID ${BuildConfig.CLIENT_ID}")
-      }
-      url(HttpRoutes.FETCH_IMAGES_URL)
+    override suspend fun getImages(): ImageResponse {
+        return client.get {
+            headers {
+                append(HttpHeaders.Authorization, "Client-ID ${BuildConfig.CLIENT_ID}")
+            }
+            url(HttpRoutes.FETCH_IMAGES_URL)
+        }.body()
     }
-  }
 }
